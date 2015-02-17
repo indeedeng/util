892b1ba315ca4b7753ef08361583ff02cc3c3d21
Jeff Plaisance <jplaisance@indeed.com>
2015-02-17 15:59:02 -0600

    faster

--- io/src/main/java/com/indeed/util/io/VIntUtils.java
+++ io/src/main/java/com/indeed/util/io/VIntUtils.java
@@ -16,15 +16,39 @@ public final class VIntUtils {
     private static final Logger log = Logger.getLogger(VIntUtils.class);
 
     public static int writeVInt(OutputStream out, int i) throws IOException {
-        // blatantly copied from lucene's IndexOutput.writeVInt();
-        int bytes = 1;
-        while ((i & ~0x7F) != 0) {
-            out.write((byte)((i & 0x7f) | 0x80));
-            i >>>= 7;
-            bytes++;
+        if (i < 0) {
+            out.write(((i&0x7F) | 0x80));
+            out.write((((i>>>7)&0x7F) | 0x80));
+            out.write((((i>>>14)&0x7F) | 0x80));
+            out.write((((i>>>21)&0x7F) | 0x80));
+            out.write((((i>>>28)&0x7F) | 0x80));
+            return 5;
+        } else if (i < 1 << 7) {
+            out.write(i);
+            return 1;
+        } else if (i < 1 << 14) {
+            out.write(((i&0x7F) | 0x80));
+            out.write((i>>>7));
+            return 2;
+        } else if (i < 1 << 21) {
+            out.write(((i&0x7F) | 0x80));
+            out.write((((i>>>7)&0x7F) | 0x80));
+            out.write((i>>>14));
+            return 3;
+        } else if (i < 1 << 28) {
+            out.write(((i&0x7F) | 0x80));
+            out.write((((i>>>7)&0x7F) | 0x80));
+            out.write((((i>>>14)&0x7F) | 0x80));
+            out.write((i>>>21));
+            return 4;
+        } else {
+            out.write(((i&0x7F) | 0x80));
+            out.write((((i>>>7)&0x7F) | 0x80));
+            out.write((((i>>>14)&0x7F) | 0x80));
+            out.write((((i>>>21)&0x7F) | 0x80));
+            out.write((i>>>28));
+            return 5;
         }
-        out.write((byte)i);
-        return bytes;
     }
 
     public static int readVInt(InputStream in) throws IOException {
@@ -48,15 +72,39 @@ public final class VIntUtils {
     }
 
     public static int writeVInt(DataOutput out, int i) throws IOException {
-        // blatantly copied from lucene's IndexOutput.writeVInt();
-        int bytes = 1;
-        while ((i & ~0x7F) != 0) {
-            out.write((byte)((i & 0x7f) | 0x80));
-            i >>>= 7;
-            bytes++;
+        if (i < 0) {
+            out.write(((i&0x7F) | 0x80));
+            out.write((((i>>>7)&0x7F) | 0x80));
+            out.write((((i>>>14)&0x7F) | 0x80));
+            out.write((((i>>>21)&0x7F) | 0x80));
+            out.write((((i>>>28)&0x7F) | 0x80));
+            return 5;
+        } else if (i < 1 << 7) {
+            out.write(i);
+            return 1;
+        } else if (i < 1 << 14) {
+            out.write(((i&0x7F) | 0x80));
+            out.write((i>>>7));
+            return 2;
+        } else if (i < 1 << 21) {
+            out.write(((i&0x7F) | 0x80));
+            out.write((((i>>>7)&0x7F) | 0x80));
+            out.write((i>>>14));
+            return 3;
+        } else if (i < 1 << 28) {
+            out.write(((i&0x7F) | 0x80));
+            out.write((((i>>>7)&0x7F) | 0x80));
+            out.write((((i>>>14)&0x7F) | 0x80));
+            out.write((i>>>21));
+            return 4;
+        } else {
+            out.write(((i&0x7F) | 0x80));
+            out.write((((i>>>7)&0x7F) | 0x80));
+            out.write((((i>>>14)&0x7F) | 0x80));
+            out.write((((i>>>21)&0x7F) | 0x80));
+            out.write((i>>>28));
+            return 5;
         }
-        out.write((byte)i);
-        return bytes;
     }
 
     public static int readVInt(DataInput in) throws IOException {
@@ -89,15 +137,82 @@ public final class VIntUtils {
     }
 
     public static int writeVInt64(OutputStream out, long i) throws IOException {
-        // blatantly copied from lucene's IndexOutput.writeVInt();
-        int bytes = 1;
-        while ((i & ~0x7F) != 0) {
-            out.write((byte)((i & 0x7f) | 0x80));
-            i >>>= 7;
-            bytes++;
+        if (i < 0) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int) (((i>>>28)&0x7F) | 0x80));
+            out.write((int) (((i>>>35)&0x7F) | 0x80));
+            out.write((int) (((i>>>42)&0x7F) | 0x80));
+            out.write((int) (((i>>>49)&0x7F) | 0x80));
+            out.write((int) (((i>>>56)&0x7F) | 0x80));
+            out.write((int) (i>>>63));
+            return 10;
+        } else if (i < 1L << 7) {
+            out.write((int)i);
+            return 1;
+        } else if (i < 1L << 14) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (i>>>7));
+            return 2;
+        } else if (i < 1L << 21) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (i>>>14));
+            return 3;
+        } else if (i < 1L << 28) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (i>>>21));
+            return 4;
+        } else if (i < 1L << 35) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int) (i>>>28));
+            return 5;
+        } else if (i < 1L << 42) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int) (((i>>>28)&0x7F) | 0x80));
+            out.write((int) (i>>>35));
+            return 6;
+        } else if (i < 1L << 49) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int) (((i>>>28)&0x7F) | 0x80));
+            out.write((int) (((i>>>35)&0x7F) | 0x80));
+            out.write((int) (i>>>42));
+            return 7;
+        } else if (i < 1L << 56) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int) (((i>>>28)&0x7F) | 0x80));
+            out.write((int) (((i>>>35)&0x7F) | 0x80));
+            out.write((int) (((i>>>42)&0x7F) | 0x80));
+            out.write((int) (i>>>49));
+            return 8;
+        } else {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int) (((i>>>28)&0x7F) | 0x80));
+            out.write((int) (((i>>>35)&0x7F) | 0x80));
+            out.write((int) (((i>>>42)&0x7F) | 0x80));
+            out.write((int) (((i>>>49)&0x7F) | 0x80));
+            out.write((int) (i>>>56));
+            return 9;
         }
-        out.write((byte)i);
-        return bytes;
     }
 
     public static long readVInt64(InputStream in) throws IOException {
@@ -121,15 +236,82 @@ public final class VIntUtils {
     }
 
     public static int writeVInt64(DataOutput out, long i) throws IOException {
-        // blatantly copied from lucene's IndexOutput.writeVInt();
-        int bytes = 1;
-        while ((i & ~0x7F) != 0) {
-            out.write((byte)((i & 0x7f) | 0x80));
-            i >>>= 7;
-            bytes++;
+        if (i < 0) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int) (((i>>>28)&0x7F) | 0x80));
+            out.write((int) (((i>>>35)&0x7F) | 0x80));
+            out.write((int) (((i>>>42)&0x7F) | 0x80));
+            out.write((int) (((i>>>49)&0x7F) | 0x80));
+            out.write((int) (((i>>>56)&0x7F) | 0x80));
+            out.write((int)(i>>>63));
+            return 10;
+        } else if (i < 1L << 7) {
+            out.write((int)i);
+            return 1;
+        } else if (i < 1L << 14) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int)(i>>>7));
+            return 2;
+        } else if (i < 1L << 21) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int)(i>>>14));
+            return 3;
+        } else if (i < 1L << 28) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int)(i>>>21));
+            return 4;
+        } else if (i < 1L << 35) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int)(i>>>28));
+            return 5;
+        } else if (i < 1L << 42) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int) (((i>>>28)&0x7F) | 0x80));
+            out.write((int)(i>>>35));
+            return 6;
+        } else if (i < 1L << 49) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int) (((i>>>28)&0x7F) | 0x80));
+            out.write((int) (((i>>>35)&0x7F) | 0x80));
+            out.write((int)(i>>>42));
+            return 7;
+        } else if (i < 1L << 56) {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int) (((i>>>28)&0x7F) | 0x80));
+            out.write((int) (((i>>>35)&0x7F) | 0x80));
+            out.write((int) (((i>>>42)&0x7F) | 0x80));
+            out.write((int)(i>>>49));
+            return 8;
+        } else {
+            out.write((int) ((i&0x7F) | 0x80));
+            out.write((int) (((i>>>7)&0x7F) | 0x80));
+            out.write((int) (((i>>>14)&0x7F) | 0x80));
+            out.write((int) (((i>>>21)&0x7F) | 0x80));
+            out.write((int) (((i>>>28)&0x7F) | 0x80));
+            out.write((int) (((i>>>35)&0x7F) | 0x80));
+            out.write((int) (((i>>>42)&0x7F) | 0x80));
+            out.write((int) (((i>>>49)&0x7F) | 0x80));
+            out.write((int)(i>>>56));
+            return 9;
         }
-        out.write((byte)i);
-        return bytes;
     }
 
     public static long readVInt64(DataInput in) throws IOException {
