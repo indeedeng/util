a757b955342b4735f81a258f4fefef244d9a3b40
darren <darren@indeed.com>
2015-04-01 12:48:16 -0500

    The patch fixes one bug and a quirk
    The bug
    --------------
    Some of the operations (invert) could lead to a ThreadSafeBitSet with a "dirty state".
    The unused bits of the last int would not be all equal to 1, which would impact
    cardinality and equals.
    
    The quirk
    -------------
    Two datasets with different "capacity" with the same integers would be equal
    iff their capacity has the same reminder modulo 32.
    After the commit, 2 datasets need to have the same capacity to be equal
    as a prerequisite.

--- util-core/src/main/java/com/indeed/util/core/threads/ThreadSafeBitSet.java
+++ util-core/src/main/java/com/indeed/util/core/threads/ThreadSafeBitSet.java
@@ -16,7 +16,28 @@ public class ThreadSafeBitSet implements Serializable {
 
     public ThreadSafeBitSet(int size) {
         this.size = size;
-        bits = new int[(size+31)>>>5];
+        this.bits = new int[(size+31)>>>5];
+    }
+
+    private ThreadSafeBitSet(int[] bits, int size) {
+        this.bits = bits;
+        this.size = size;
+    }
+
+    /* mask for the last byte that contains 1 on the bits that
+     * are actually in use.
+     */
+    private int finalIntUsedBitMask() {
+        final int nbUsedBitsInLastInt = this.size % 32;
+        final int nbUselessBits = (32 - nbUsedBitsInLastInt) % 32;
+        return (~0 >>> nbUselessBits);
+    }
+
+    private void cleanUpLastInt() {
+        final int nbUsedBitsInLastInt = this.size % 32;
+        if (nbUsedBitsInLastInt > 0) {
+            this.bits[this.bits.length - 1] &= finalIntUsedBitMask();
+        }
     }
 
     public final void set(int index, boolean value) {
@@ -27,6 +48,10 @@ public class ThreadSafeBitSet implements Serializable {
     public final boolean get(int index) {
         final int t = 1<<(index&0x1F);
         return (bits[index>>5]&t) != 0;
+    }
+
+    public ThreadSafeBitSet copy() {
+        return new ThreadSafeBitSet(this.bits.clone(), this.size);
     }
 
     public final int size() {
@@ -47,12 +72,14 @@ public class ThreadSafeBitSet implements Serializable {
 
     public final void setAll() {
         Arrays.fill(bits, 0xFFFFFFFF);
+        cleanUpLastInt();
     }
 
     public final void invertAll() {
         for (int i = 0; i < bits.length; i++) {
             bits[i] = ~bits[i];
         }
+        cleanUpLastInt();
     }
 
     // basically same as java's BitSet.and
@@ -84,13 +111,91 @@ public class ThreadSafeBitSet implements Serializable {
     }
 
     public final void copyFrom(ThreadSafeBitSet other) {
-        System.arraycopy(other.bits, 0, bits, 0, other.bits.length);
+        if (other.size == this.size) {
+            System.arraycopy(other.bits, 0, bits, 0, other.bits.length);
+        }
+        else if (other.size < this.size) {
+            if (other.size > 0) {
+                // we copy the array but handle the last int separately
+                System.arraycopy(other.bits, 0, bits, 0, other.bits.length - 1);
+                final int otherLastInt = other.bits[other.bits.length - 1];
+                final int myLastModifiedInt = this.bits[other.bits.length - 1];
+                final int mask = other.finalIntUsedBitMask();
+                final int lastInt = (mask & otherLastInt) | ((~mask) & myLastModifiedInt);
+                this.bits[other.bits.length - 1] = lastInt;
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Copy from array bigger than destination is forbidden");
+        }
     }
+
+    public final void copyFromRange(final ThreadSafeBitSet other, final int startIndex, final int otherStartIndex, final int length) {
+
+        if (length == 0) {
+            return;
+        }
+
+        // end indices are INCLUSIVE to make the twiddling a tad simpler
+        final int endIndex = startIndex + length - 1;
+        final int otherEndIndex = otherStartIndex + length - 1;
+
+        final int bitsStartIndex = startIndex >>> 5;
+        final int otherBitsStartIndex = otherStartIndex >>> 5;
+
+        final int bitsEndIndex = endIndex >>> 5;
+
+        if ((startIndex & 0x1F) == (otherStartIndex & 0x1F)) {
+            if (bitsStartIndex != bitsEndIndex) {
+                System.arraycopy(other.bits, otherBitsStartIndex + 1, bits, bitsStartIndex + 1, bitsEndIndex - bitsStartIndex - 1);
+
+                simpleCopyFromRange(other, startIndex, otherStartIndex, Integer.SIZE - (startIndex & 0x1F));
+                simpleCopyFromRange(other, endIndex & ~0x1F, otherEndIndex & ~0x1F, (endIndex & 0x1F) + 1);
+            } else {
+                simpleCopyFromRange(other, startIndex, otherStartIndex, length);
+            }
+        } else {
+            if (bitsStartIndex != bitsEndIndex) {
+                final int difference = (startIndex & 0x1F) - (otherStartIndex & 0x1F);
+                final int absDifference = Math.abs(difference);
+                final int reverseDifference = Integer.SIZE - absDifference;
+                final int reverseDifferenceMask = -1 << reverseDifference;
+                final int absDifferenceMask = -1 << absDifference;
+
+                if (difference > 0) {
+                    bits[bitsStartIndex] = (bits[bitsStartIndex] & ~(-1 << (startIndex & 0x1F))) |
+                            ((other.bits[otherBitsStartIndex] & (-1 << (otherStartIndex & 0x1F))) << difference);
+                    for (int bitsIndex = bitsStartIndex + 1, otherBitsIndex = otherBitsStartIndex; bitsIndex < bitsEndIndex; ++bitsIndex, ++otherBitsIndex) {
+                        bits[bitsIndex] = ((other.bits[otherBitsIndex] & reverseDifferenceMask) >>> reverseDifference) |
+                                ((other.bits[otherBitsIndex + 1] & ~reverseDifferenceMask) << difference);
+                    }
+                    simpleCopyFromRange(other, endIndex & ~0x1F, otherEndIndex - (endIndex & 0x1F), (endIndex & 0x1F) + 1);
+                } else {
+                    simpleCopyFromRange(other, startIndex, otherStartIndex, Integer.SIZE - (startIndex & 0x1F));
+                    for (int bitsIndex = bitsStartIndex + 1, otherBitsIndex = otherBitsStartIndex + 1; bitsIndex < bitsEndIndex; ++bitsIndex, ++otherBitsIndex) {
+                        bits[bitsIndex] = ((other.bits[otherBitsIndex] & absDifferenceMask) >>> absDifference) |
+                                ((other.bits[otherBitsIndex + 1] & ~absDifferenceMask) << reverseDifference);
+                    }
+                    simpleCopyFromRange(other, endIndex & ~0x1F, otherEndIndex - (endIndex & 0x1F), (endIndex & 0x1F) + 1);
+                }
+            } else {
+                simpleCopyFromRange(other, startIndex, otherStartIndex, length);
+            }
+        }
+    }
+
+    private void simpleCopyFromRange(final ThreadSafeBitSet other, final int startIndex, final int otherStartIndex, final int length) {
+        for (int i = 0; i < length; ++i) {
+            set(startIndex + i, other.get(otherStartIndex + i));
+        }
+    }
+
 
     public final int cardinality() {
         int sum = 0;
-        for (int x : bits)
+        for (final int x : bits) {
             sum += Integer.bitCount(x);
+        }
         return sum;
     }
 
@@ -104,6 +209,7 @@ public class ThreadSafeBitSet implements Serializable {
     public static boolean equals(ThreadSafeBitSet bitset1, ThreadSafeBitSet bitset2) {
         if (bitset1 == bitset2) return true;
         if (bitset1 == null || bitset2 == null) return false;
+        if (bitset1.size() != bitset2.size()) return false;
         return Arrays.equals(bitset1.bits, bitset2.bits);
     }
 
--- util-core/src/test/java/com/indeed/util/core/threads/TestThreadSafeBitSet.java
+++ util-core/src/test/java/com/indeed/util/core/threads/TestThreadSafeBitSet.java
@@ -11,6 +11,8 @@ import com.indeed.util.core.TreeTimer;
  * @author ahudson
  */
 public class TestThreadSafeBitSet extends TestCase {
+    private final Random rand = new Random();
+
     public void testFunctionality() {
         for (int size = 5000; size < 6000; size++) {
             ThreadSafeBitSet bitSet = new ThreadSafeBitSet(size);
@@ -40,18 +42,22 @@ public class TestThreadSafeBitSet extends TestCase {
         }
     }
 
+
     public void testCopyFrom() {
         ThreadSafeBitSet bs = new ThreadSafeBitSet(100);
         for (int i = 0; i < bs.size(); i++) {
-            if (i%7 == 0) bs.set(i);
+            if (i%3 == 0) bs.set(i);
         }
         ThreadSafeBitSet bs2 = new ThreadSafeBitSet(200);
-        for (int i = 0; i < bs.size(); i++) {
-            bs2.set(i, !bs.get(i));
+        for (int i = 0; i < bs2.size(); i++) {
+            if (i%2 == 0) bs2.set(i);
         }
         bs2.copyFrom(bs);
-        for (int i = 0; i < bs2.size(); i++) {
-            assertEquals(i < 100 && i % 7 == 0, bs2.get(i));
+        for (int i = 0; i < 100; i++) {
+            assertEquals("Failed for " + i, i % 3 == 0, bs2.get(i));
+        }
+        for (int i = 100; i < 200; i++) {
+            assertEquals("Failed for " + i, i % 2 == 0, bs2.get(i));
         }
     }
 
@@ -103,6 +109,39 @@ public class TestThreadSafeBitSet extends TestCase {
         b.or(a);
         for (int i = 0; i < 200; i++) assertEquals(""+i, b.get(i), i < 100);
     }
+
+
+    public void testCardinality() {
+        for (int i=0; i<32; i++) {
+            final ThreadSafeBitSet smallBitSet = new ThreadSafeBitSet(i);
+            smallBitSet.setAll();
+            final int cardinality = smallBitSet.cardinality();
+            assertEquals(smallBitSet.cardinality(), cardinality);
+        }
+    }
+
+    public void testInverse() {
+        final Random rand = new Random(42);
+        final int size = 32 * 10 + 5;
+        final ThreadSafeBitSet randomSet = new ThreadSafeBitSet(size);
+        for (int i=0; i < size; i++) {
+            if (rand.nextDouble() < 0.2) {
+                randomSet.set(i);
+            }
+        }
+        final int cardinality = randomSet.cardinality();
+        final ThreadSafeBitSet invRandomSet = randomSet.copy();
+        invRandomSet.invertAll();
+        for (int i = 0; i < size; i++) {
+            assertEquals(!invRandomSet.get(i), randomSet.get(i));
+        }
+        final int invCardinality = invRandomSet.cardinality();
+        assertEquals(invCardinality, size - cardinality);
+        invRandomSet.xor(randomSet);
+        assertEquals(invRandomSet.cardinality(), size);
+    }
+
+
 
     public void testXor() {
         ThreadSafeBitSet a = new ThreadSafeBitSet(200);
@@ -250,6 +289,96 @@ public class TestThreadSafeBitSet extends TestCase {
         result = ThreadSafeBitSet.or(a, b);
         assertEquals(200, result.size());
         for (int i = 0; i < 200; i++) assertEquals(""+i, result.get(i), i < 100);
+    }
+
+    public void testCopyFromRange() {
+        // start at 0 in both
+        copyFromRangeCase(0, 0, 3);
+        copyFromRangeCase(0, 0, 20);
+        copyFromRangeCase(0, 0, 31);
+        copyFromRangeCase(0, 0, 32);
+        copyFromRangeCase(0, 0, 33);
+        copyFromRangeCase(0, 0, 63);
+        copyFromRangeCase(0, 0, 64);
+        copyFromRangeCase(0, 0, 65);
+
+        // not starting at 0 but int-aligned
+        copyFromRangeCase(1, 65, 7);
+        copyFromRangeCase(1, 65, 8);
+        copyFromRangeCase(1, 65, 9);
+        copyFromRangeCase(1, 65, 15);
+        copyFromRangeCase(1, 65, 16);
+        copyFromRangeCase(1, 65, 17);
+        copyFromRangeCase(1, 65, 31);
+        copyFromRangeCase(1, 65, 32);
+        copyFromRangeCase(1, 65, 33);
+        copyFromRangeCase(1, 65, 63);
+        copyFromRangeCase(1, 65, 64);
+        copyFromRangeCase(1, 65, 65);
+        copyFromRangeCase(1, 65, 80);
+
+        // not starting at 0, not aligned
+        for (int i = 1; i < 100; ++i) {
+            copyFromRangeCase(33, 95, i);
+            copyFromRangeCase(95, 33, i);
+
+            copyFromRangeCase(32, 95, i);
+            copyFromRangeCase(95, 32, i);
+
+            copyFromRangeCase(33, 96, i);
+            copyFromRangeCase(96, 33, i);
+        }
+    }
+
+    private void copyFromRangeCase(final int start, final int otherStart, final int length) {
+        final ThreadSafeBitSet dest = new ThreadSafeBitSet(start + length + 200);
+        final ThreadSafeBitSet source = new ThreadSafeBitSet(otherStart + length + 200);
+
+        for (int i = 0; i < 20; ++i) {
+            final ThreadSafeBitSet expected = new ThreadSafeBitSet(dest.size());
+            dest.clearAll();
+            source.clearAll();
+
+            for (int index = 0; index < start; ++index) {
+                final boolean b = rand.nextBoolean();
+                expected.set(index, b);
+                dest.set(index, b);
+            }
+
+            for (int index = start; index < start + length; ++index) {
+                dest.set(index, rand.nextBoolean());
+            }
+
+            for (int index = start + length; index < dest.size(); ++index) {
+                final boolean b = rand.nextBoolean();
+                expected.set(index, b);
+                dest.set(index, b);
+            }
+
+            for (int index = 0; index < otherStart; ++index) {
+                source.set(index, rand.nextBoolean());
+            }
+
+            for (int index = otherStart; index < otherStart + length; ++index) {
+                final boolean b = rand.nextBoolean();
+                expected.set(index - otherStart + start, b);
+                source.set(index, b);
+            }
+
+            for (int index = otherStart + length; index < source.size(); ++index) {
+                source.set(index, rand.nextBoolean());
+            }
+
+            dest.copyFromRange(source, start, otherStart, length);
+
+            for (int index = 0; index < dest.size(); ++index) {
+                assertEquals(
+                        "mismatch at index " + index + " with start=" + start + ", otherStart=" + otherStart + ", length=" + length,
+                        expected.get(index),
+                        dest.get(index)
+                );
+            }
+        }
     }
 
     private static class ReaderThread implements Runnable {
